a9e9c84c7dd7d104098ca07ed41ba76beb921f60
Abduraxmonnn <abduraxmonasatullayev35@gmail.com>
2026-02-13 20:38:50 +0500

    feat(filter & search): added search and filter for user and post

--- apps/main/post/api/list_and_create.py
+++ apps/main/post/api/list_and_create.py
@@ -1,7 +1,11 @@
+# Django
+from django_filters.rest_framework import DjangoFilterBackend
+
 # Rest-Framework
 from rest_framework.permissions import AllowAny
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.mixins import ListModelMixin, CreateModelMixin
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 # Project
 from apps.main.post.models import Post
@@ -12,4 +16,12 @@ class PostListOrCreateViewSet(ListModelMixin, GenericViewSet):
     queryset = Post.objects.select_related('author_id')
     serializer_class = PostListOrCreateSerializer
     permission_classes = [AllowAny]
-    http_method_names = ('get', )
+    http_method_names = ('get',)
+
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    search_fields = [
+        'title',
+        'content',
+    ]
+    ordering_fields = ['created_at']
+    ordering = ['-id']
--- apps/user/api/all_list.py
+++ apps/user/api/all_list.py
@@ -1,7 +1,9 @@
 # Django
 from django.contrib.auth import get_user_model
+from django_filters.rest_framework import DjangoFilterBackend
 
 # Rest-Framework
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.permissions import AllowAny
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.mixins import ListModelMixin
@@ -17,3 +19,12 @@ class UserAllListViewSet(ListModelMixin, GenericViewSet):
     serializer_class = UserAllListSerializer
     permission_classes = [AllowAny]
     http_method_names = ('get',)
+
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    search_fields = [
+        'username',
+        'full_name',
+        'email',
+    ]
+    ordering_fields = ['username', 'full_name']
+    ordering = ['-id']
